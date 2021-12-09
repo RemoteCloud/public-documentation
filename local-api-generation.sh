63dbaef5c6817f5b2240380b636ba281a46d85a9
Jens Willmer <info@jwillmer.de>
2021-12-09 17:12:49 +0100

    local api generation script

---
+++ local-api-generation.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+RESTORE_COLOR='\033[0m'
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\033[00;33m'
+LIGHTGRAY='\033[00;37m'
+CYAN='\033[00;36m'
+
+ENDPOINTS="
+    usermanagement_service 
+    usermanagement_oauth 
+    documentation 
+    flows 
+    templates 
+    pdf-generator 
+    notifications 
+    remote-execution"
+    
+for endpoint in $ENDPOINTS 
+do 
+  widdershins ./swagger-definitions/${endpoint}.json --language_tabs shell:Shell http:HTTP javascript:JS php:PHP csharp:C# java:Java -o ./swagger-definitions/${endpoint}.md
+done
+
+cd slate
+
+for endpoint in $ENDPOINTS
+do   
+  cp ../swagger-definitions/${endpoint}.md ../slate/source/index.html.md
+  bundle exec middleman build --clean --build-dir ../mkdocs/docs/api/${endpoint}
+done
